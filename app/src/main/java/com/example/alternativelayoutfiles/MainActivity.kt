6c230a1d6606ddb1dd6fdc3ecdb2e9ca1e9a164d
M Saif Ullah <chsaif369@gmail.com>
2020-04-10 01:49:54 +0500

    Dual Pane UI for Smartphone & Tablets

--- app/src/main/java/com/example/alternativelayoutfiles/MainActivity.kt
+++ app/src/main/java/com/example/alternativelayoutfiles/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.alternativelayoutfiles
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.alternativelayoutfiles.adapters.MyDataAdapter
@@ -19,11 +18,9 @@ class MainActivity : AppCompatActivity(), ICityDataListener {
 
         setSupportActionBar(toolbar)
 
-        val viewGroup = findViewById<ViewGroup>(R.id.details_fragment_container)
+//        val viewGroup = findViewById<ViewGroup>(R.id.details_fragment_container)
 
-        if (viewGroup != null) {
-            usingTablets = true
-        }
+        usingTablets = resources.getBoolean(R.bool.using_dual_pane)
 
         Toast.makeText(this, "Wide Screen? $usingTablets", Toast.LENGTH_LONG).show()
 
